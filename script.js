cbd38f4ffbcf4ab42d0dfec78d14c1632278a6cb
Antares <enteringvalhalla@gmail.com>
2022-09-02 01:53:57 +0100

    Buttons display digit selected

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const substract = function(a, b) {
 }
 
 const divide = function(a, b) {
-    return a > b ? a / b: b / a;
+    return a / b;
 }
 
 const multiply = function(a, b) {
@@ -29,3 +29,28 @@ const operate = function(operator, a, b) {
     }
     else return "error";
 }
+
+let displayValue = [0];
+
+const displayHandle = document.getElementById('display');
+
+const displaySelection = function(value) {
+    if (displayHandle.innerHTML.length >= 18) {
+        return;
+    } else {
+    displayHandle.innerHTML += value;
+    }
+}
+
+const oneDigit = document.getElementById("one").addEventListener("click", () => displaySelection('1'));
+const twoDigit = document.getElementById("two").addEventListener("click", () => displaySelection('2'));
+const threeDigit = document.getElementById("three").addEventListener("click", () => displaySelection('3'));
+const fourDigit = document.getElementById("four").addEventListener("click", () => displaySelection('4'));
+const fiveDigit = document.getElementById("five").addEventListener("click", () => displaySelection('5'));
+const sixDigit = document.getElementById("six").addEventListener("click", () => displaySelection('6'));
+const sevenDigit = document.getElementById("seven").addEventListener("click", () => displaySelection('7'));
+const eightDigit = document.getElementById("eight").addEventListener("click", () => displaySelection('8'));
+const nineDigit = document.getElementById("nine").addEventListener("click", () => displaySelection('9'));
+const zeroDigit = document.getElementById("zero").addEventListener("click", () => displaySelection('0'));
+
+// const clearDigit = document.getElementById("clear").addEventListener("click", () => displaySelection('back'));
