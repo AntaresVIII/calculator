40339f6b880f3216976bcc1103e10582b6086e54
Antares <enteringvalhalla@gmail.com>
2022-09-08 05:53:25 +0100

    Working operations, with character limit checker and second display in progress

--- script.js
+++ script.js
@@ -1,68 +1,109 @@
-let displayValue = [];
-let operatorSelection = [];
+/*     function endsWithNumber(str) {
+        return /[0-9]+$/.test(str);
+    } */
 
-const displayHandle = document.getElementById('display');
+// Values & Operator //
+
+let firstValue = '';
+let secondValue = '';
+let operatorSelected = '';
+let previousCalc = '';
+let currentDisplay = '';
+let isOffLimits = false;
+
+// Functions //
+
+const displayHandle = document.getElementById('mainDisplay');
+const sndDisplayHandle = document.getElementById('sndDisplay');
+
+const charLimit = function() { // Checks whether the character limit on the display (17) has been reached
+    if (displayHandle.innerHTML.length >= 18) {
+        firstValue = '';
+        previousCalc = '';
+        displayHandle.innerHTML = 'Limit exceeded!';
+        isOffLimits = true;
+    } else '';
+}
 
 const displaySelection = function(value) {
-    function endsWithNumber(str) {
-        return /[0-9]+$/.test(str);
+    if (isOffLimits === true) { displayHandle.innerHTML = ''; isOffLimits = false; } else;
+    if (value === '+' || value === '-' || value === '÷' || value === 'x') {
+        if (firstValue.toString().length === 0) { return }
+        else if (secondValue.toString().length === 0 && !operatorSelected) {
+            operatorSelected = value;
+            displayHandle.innerHTML += value;  
+        }
+        else { return }
     }
-    if (displayHandle.innerHTML.length >= 17) {
+    else {
+        if (firstValue.toString().length === 0) {
+            firstValue = firstValue += value;
+            displayHandle.innerHTML += value;
+        }
+        else if (firstValue && !operatorSelected) {
+            firstValue = firstValue += value; displayHandle.innerHTML += value;
+        }
+        else {
+            secondValue = secondValue += value; 
+            displayHandle.innerHTML += value; }
+    }
+    charLimit()
+}
+/*     if (displayHandle.innerHTML.length >= 17) {  // Character limit on display set to 17
         return;
     } else if (value === '+' || value === '-' || value === '÷' || value === 'x') {
         if (displayHandle.innerHTML.length <= 0) {
             return
-        } else if (endsWithNumber(displayHandle.textContent)) {
+        } else if (endsWithNumber(displayHandle.textContent)) { // Checks wether an operator has been set
             displayHandle.innerHTML += value;
-            operatorSelection.push(value);
+            operatorSelected.push(value);
 
          } else { return }
     }
      else {
     displayHandle.innerHTML += value;
-    displayValue.push(value);
+    firstValue.push(value);
     }
-}
+} */
 
-const clearLastDigit = function() {
+const clearLastDigit = function() { // WORK IN PROGRESS //
     if (displayHandle.innerHTML.length >= 1) {
+        if ( secondValue.toString().length === 0) {}
         displayHandle.innerHTML = displayHandle.textContent.slice(0, -1);
     }
     else return;
 }
 
-const add = function(a, b) {
-    displayHandle.innerHTML = a + b;
-    console.log(add);
-}
-
-const substract = function(a, b) {
-    displayHandle.innerHTML = a - b;
-}
-
-const divide = function(a, b) {
-    displayHandle.innerHTML = a / b;
-}
-
-const multiply = function(a, b) {
-    displayHandle.innerHTML = a * b;
-}
-
 const operate = function(operator, a, b) {
+    if (!operatorSelected) { return }
+    else {
+    a = Number(a);
+    b = Number(b);
     if (operator === '+') {
-        return add(a, b);
+        displayHandle.innerHTML = a + b;
+        previousCalc = a + b;
     }
     else if (operator === '-') {
-        return substract(a, b);
+        displayHandle.innerHTML = a - b;
+        previousCalc = a - b;
     }
     else if (operator === '÷') {
-        return divide(a, b);
+        displayHandle.innerHTML = a / b;
+        previousCalc = a / b;
     }
     else if (operator === 'x') {
-        return multiply(a, b);
+        displayHandle.innerHTML = a * b;
+        previousCalc = a * b;
     }
-    else return "error";
-}
+    else;
+    firstValue = previousCalc;
+    secondValue = '';
+    operatorSelected = '';
+    sndDisplayHandle.innerHTML = previousCalc;
+    charLimit();
+}}
+
+// Buttons with Event Listeners //
 
 const oneDigit = document.getElementById("one").addEventListener("click", () => displaySelection('1'));
 const twoDigit = document.getElementById("two").addEventListener("click", () => displaySelection('2'));
@@ -81,4 +122,5 @@ const minusDigit = document.getElementById("minus").addEventListener("click", ()
 const dividerDigit = document.getElementById("divider").addEventListener("click", () => displaySelection('÷'));
 const multiplierDigit = document.getElementById("multiplier").addEventListener("click", () => displaySelection('x'));
 
-const result = document.getElementById("equal").addEventListener("click", () => operate(operatorSelection, displayValue[0], displayValue[1]));
+const result = document.getElementById("equal").addEventListener("click", () => 
+    operate(operatorSelected, firstValue, secondValue));
